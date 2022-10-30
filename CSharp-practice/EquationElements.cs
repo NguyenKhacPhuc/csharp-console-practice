6ae346c23e72fa5fa416c661237850c3452a54a2
phucnguyenk <nguyenkhacphuc311@gmail.com>
2022-10-30 23:47:17 +0700

    degree equation resolved.

---
+++ CSharp-practice/EquationElements.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DegreeEquationResolver
+{
+    internal class EquationElements
+    {
+        private  double numberA = 0;
+        private  double numberB = 0;
+        private  double numberC = 0;
+        public EquationElements(double numberA, double numberB, double numberC)
+        {
+            this.numberA = numberA;
+            this.numberB = numberB;
+            this.numberC = numberC;
+        }
+
+        public double NumberA
+        {
+            get { return numberA; }
+            set { numberA = value; }
+        }
+        public double NumberB
+        {
+            get { return numberB; } 
+            set { numberB = value; }
+        }
+        public double NumberC
+        {
+            get { return numberC; }
+            set { numberC = value; }
+        }
+
+    }
+
+    internal class EquationResult 
+    {
+        private readonly double result1;
+        private readonly double result2;
+
+        public EquationResult(double result1, double result2)
+        {
+            this.result1 = result1;
+            this.result2 = result2;
+        }
+
+        public EquationResult(double result1)
+        {
+            this.result1 = result1;
+        }
+
+        public double Result1
+        {
+            get { return result1; }
+        }
+        public double Result2
+        {
+            get { return result2; }
+        }
+    }
+}
--- CSharp-practice/Program.cs
+++ CSharp-practice/Program.cs
@@ -1,2 +1,78 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using DegreeEquationResolver;
+
+using System;
+
+namespace DegreeEquationResolver
+{
+    class Program
+    {
+        private const float NO_RESULT = -1f;
+        static void Main(string[] args)
+        {
+            var equationElements = getUserInput();
+            Console.WriteLine(equationElements);
+            var result = caculatedEquation(equationElements);
+            if (result == null)
+            {
+                Console.WriteLine("No result");
+            }
+            else
+            {
+                Console.WriteLine("The result is " + result.Result1 + " And " + result.Result2);
+            }
+        }
+
+        static EquationElements getUserInput()
+        {
+            Console.WriteLine("Please enter a: ");
+            double numberA = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please enter b: ");
+            double numberB = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please enter c: ");
+            double numberC = Convert.ToDouble(Console.ReadLine());
+
+            return new EquationElements(numberA, numberB, numberC);
+        }
+
+        static EquationResult? caculatedEquation(EquationElements equationElements)
+        {
+            double numberA = equationElements.NumberA;
+            double numberB = equationElements.NumberB;
+            double numberC = equationElements.NumberC;
+
+            if (numberA == 0 && numberB == 0)
+            {
+                return null;
+            }
+            else if (numberA == 0 && numberB != 0)
+            {
+                return new EquationResult(-numberC / numberB);
+            }
+            else
+            {
+                double delta = numberB * numberB - 4 * numberA * numberC;
+                Console.WriteLine("Delta " + delta);
+
+                if (delta > 0)
+                {
+                    return new EquationResult(((-numberB + Math.Sqrt(delta)) / (2 * numberA)), ((-numberB - Math.Sqrt(delta)) / (2 * numberA)));
+                }
+                else if (delta == 0)
+                {
+                    return new EquationResult((-numberB / (2 * numberA)));
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
